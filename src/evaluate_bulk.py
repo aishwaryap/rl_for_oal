1bae5f317be8075e08a0ee76d6b67506a4d1dbb4
Aishwarya Padmakumar <aish@cs.utexas.edu>
2018-02-17 15:51:47 -0600

    Bug fxes and results of first bulk eval

--- src/evaluate_bulk.py
+++ src/evaluate_bulk.py
@@ -8,26 +8,36 @@ import re
 
 def main(args):
     with open(args.completed_test_file) as handle:
-        lines = handle.read().split('\n')
+        lines = handle.read().strip().split('\n')
         agent_names = [re.sub('_test.sh', '', line.strip()) for line in lines]
 
     all_avg_success = dict()
     all_avg_len = dict()
 
     for agent_name in agent_names:
-        dialog_stats_file = os.path.join(*[args.output_path, agent_name + '_test', 'test_stats.txt'])
+        print 'agent name =', agent_name
+        # dialog_stats_file = os.path.join(*[args.agents_path, agent_name + '_test', 'test_stats.txt'])
+        dialog_stats_file = os.path.join(*[args.agents_path, agent_name, 'train_stats.txt'])        
         avg_success = list()
         avg_len = list()
 
         with open(dialog_stats_file) as handle:
             reader = csv.reader(handle, delimiter=',')
-            stats = list()
+            all_stats = list()
             for row in reader:
-                stats.append(row)
+                all_stats.append(row)
 
-        num_batches = len(stats) / args.averaging_beam
+        num_batches = len(all_stats) / args.averaging_beam
+
+        print 'len(stats) =', len(all_stats)
+        print 'num_batches =', num_batches
+
         for batch_num in range(num_batches):
-            batch_stats = stats[batch_num * args.averaging_beam: (batch_num + 1) * args.averaging_beam]
+            start = batch_num * args.averaging_beam
+            end = (batch_num + 1) * args.averaging_beam
+            print 'batch_num =', batch_num, ', start =', start, ', end =', end
+            batch_stats = all_stats[start:end]
+            print 'agent_name =', agent_name, ', batch_stats:',len(batch_stats)
             avg_success.append(sum([float(stats[2]) for stats in batch_stats]) / float(args.averaging_beam))
             avg_len.append(sum([float(stats[3]) for stats in batch_stats]) / float(args.averaging_beam))
 
